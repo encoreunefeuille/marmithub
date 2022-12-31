990799c79956a1fe714f2b43c9602ac005b99407
Guillaume Bouvignies <guillaumebouvignies@gmail.com>
2022-12-31 16:59:15 +0100

    Add check-links action (#44)
    * Add check-links action
    
    * Add check-links.sh
    
    * Fix action
    
    * check with broken link
    
    * Fix broken link

---
+++ scripts/check-links.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -euo pipefail
+
+FOLDERS=(
+    './recettes/sucrees/'
+    './recettes/salees/'
+)
+EXTENSION='.md'
+
+exit_code=0
+for folder in ${FOLDERS[@]}; do
+    # echo "Checking folder ${folder}"
+    for file in $(ls ${folder} | grep ${EXTENSION}); do
+        file_path=${folder}${file}
+        # echo "Checking file ${file_path}"
+        if [[ ! "$(cat README.md | grep -c ${file_path})" -eq 1 ]]; then
+            echo "No links found for file ${file_path}" 
+            exit_code=1
+        fi
+    done
+done
+echo "Returning exit code $exit_code"
+exit $exit_code
